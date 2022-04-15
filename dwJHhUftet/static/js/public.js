2d4af2bacfd47905545392a0257d4d492eedaa00
majunjie58 <ma.junjie@decathlon.com>
2022-04-15 11:23:58 +0800

    update

---
+++ dwJHhUftet/static/js/public.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+
+//文档加载就绪
+// $(document).ready(function(){
+
+// globalAPI.eventTop();
+
+// globalAPI.eventFoot();
+
+// if( globalAPI.judegMobileBrowser() ){
+//   //手机浏览器下 位置修正
+//   $(".common_crumb-icon").css({"margin-top":"-0.1rem"});
+// }
+
+
+// });//end of $(document).ready
+
+// //页面全部加载完成 包括图片
+// window.onload=function(){
+
+
+
+// };
